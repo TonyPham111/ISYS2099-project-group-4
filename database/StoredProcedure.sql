1c658e33fb89773b492e51856a1258c522ef8f8c
Ly Minh Khoi <lyminhkhoi3804@gmail.com>
2024-08-21 23:25:54 +0700

    adding procedure for registering new User

--- database/StoredProcedure.sql
+++ database/StoredProcedure.sql
@@ -83,6 +83,30 @@ BEGIN
 
 END $$
 
+DELIMITER $$
+
+CREATE PROCEDURE RegisterUser(
+    IN p_username VARCHAR(255),
+    IN p_password VARCHAR(255),
+    IN p_role VARCHAR(50),
+    IN p_department_name VARCHAR(255)
+)
+BEGIN
+    DECLARE department_id INT;
+
+    -- Check if the department exists
+    SELECT id INTO department_id FROM Departments WHERE name = p_department_name;
+    IF department_id IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Invalid department name provided';
+    END IF;
+
+    -- Insert the new user
+    INSERT INTO staff (username, password, role, department_id)
+    VALUES (p_username, p_password, p_role, department_id);
+END $$
+
+DELIMITER ;
 
 
 
