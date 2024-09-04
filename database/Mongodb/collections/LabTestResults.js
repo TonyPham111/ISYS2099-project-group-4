4e90d28497ad5a96c82e1afb7ff4b63fa1b995b4
Ly Minh Khoi <lyminhkhoi3804@gmail.com>
2024-09-04 14:43:21 +0700

    update appointment notes methods and test results collection

--- database/Mongodb/collections/LabTestResults.js
+++ database/Mongodb/collections/LabTestResults.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
-
-
 // Test Document
-const testDocument = new mongoose.Schema({
+const testDocumentSchema = new mongoose.Schema({
     _id: String,
     lab_result_document: String,
     sample_image: [String],
 }, { timestamps: true });
 
-
-const TestResult = mongoose.model('TestResult', LabTestResult);
+const TestResult = mongoose.model('TestResult', testDocumentSchema);
 module.exports = TestResult;
